878e8066713caeb9ea039e9d359d396dd97a8972
Vladyslav Sahaidak <svg.231@gmail.com>
2023-09-23 00:22:50 +0200

    'myfinnalycommit'

--- js/task-06.js
+++ js/task-06.js
@@ -4,3 +4,4 @@ inputEl.addEventListener('input', (event) => {
     event.currentTarget.value.length === 6 ? inputEl.classList.replace('invalid', 'valid') : inputEl.classList.add('invalid');
 })
 
+console.log(`asdf`)
